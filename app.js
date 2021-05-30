7b6607e7aa79a83b103e47ef018094b386872761
Jay Thomas <thelastjet17@gmail.com>
2021-05-29 22:07:23 -0700

    completing exercises 1,2, and 3

--- app.js
+++ app.js
@@ -8,12 +8,57 @@ console.log("EXERCISE 1:\n==========\n");
 
 // YOUR CODE HERE
 
+let a = 20;
+let b = 4;
+let add = a + b;
+let minus = a - b;
+let multiply = a * b;
+let dividing = a / b;
+
+console.log(a);
+console.log(b);
+console.log(add);
+console.log(minus);
+console.log(multiply);
+console.log(dividing);
+
 // Exercise 2
 console.log("EXERCISE 2:\n==========\n");
 
 // YOUR CODE HERE
 
+let c = 11;
+let str = "11";
+let str2 = "eleven";
+let isPresent = true;
+let firstName = "Jackie";
+let lastName = "Chan";
+
+console.log(c + str);
+console.log(c + str2);
+console.log(c + isPresent);
+console.log(c + firstName);
+console.log(c + lastName);
+
+
 // Exercise 3
 console.log("EXERCISE 3:\n==========\n");
 
 // YOUR CODE HERE
+
+let d = 5;
+let str3 = "5";
+let str4 = "five";
+let isPresent2 = "false";
+let firstName2 = "Robin";
+let lastName2 = "Williams";
+
+console.log(d == str3);
+console.log(d === str4);
+console.log(!isPresent2);
+console.log("eleven" == str3 && d >= str4);
+console.log(!isPresent2 || isPresent2);
+console.log(0 == false);
+console.log(0 === false);
+console.log(0 != false);
+console.log(0 !== false);
